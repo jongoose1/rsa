c588916df524972aafa6aabd99c84ab200562e70
Jon Goose <jongoose69@gmail.com>
2024-12-01 13:59:39 -0800

    ok

--- src/rsa.h
+++ src/rsa.h
@@ -11,7 +11,7 @@ Number of 4 byte words in a bignum
 512 -> 8192 bit modulus
 1024 -> 16384 bit modulus
 */
-#define NWORDS 64
+#define NWORDS 256
 
 /* Number of 4 byte words used in padding */
 #define NPADDING (NWORDS/128)
