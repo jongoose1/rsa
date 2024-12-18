a918d9368fb513d8c6339a3cc211fbe74989a9ea
Jon Goose <jongoose69@gmail.com>
2024-12-17 17:46:03 -0800

    ok

--- test/random_test.c
+++ test/random_test.c
@@ -12,6 +12,12 @@ int main(int argc, char ** argv) {
 
 	bignum a = bignum_random();
 	bignum_print(&a);
+	a = bignum_half_random();
+	bignum_print(&a);
+	a = bignum_quarter_random();
+	bignum_print(&a);
+	bignum_pad(&a);
+	bignum_print(&a);
 
 	close(dr);
 
